ab5fc3062c7dae5a50096cbca964eae6b097c86e
SOME <sourbar2001@gmail.com>
2023-05-03 01:35:18 -0700

    app.py update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''Contains a Flask web application API.
 '''
-import os
+from os import getenv
 from flask import Flask, jsonify
 from flask_cors import CORS
 
@@ -11,8 +11,6 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 '''The Flask web application instance.'''
-app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-app_port = int(os.getenv('HBNB_API_PORT', '5000'))
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 CORS(app, resources={'/*': {'origins': app_host}})
@@ -40,7 +38,11 @@ def error_400(error):
     return jsonify(error=msg), 400
 
 
-if __name__ == '__main__':
-    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(host=app_host, port=app_port, threaded=True)
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
